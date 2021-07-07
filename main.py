379efc791bab470bb46ef5748f7ce1efe6b10e77
Jeff Martin <chess.logic@gmail.com>
2021-07-07 15:47:16 -0700

    Update infrastructure
    * In accordance with the last commit...
    * Update the flask app (remove /results routing)
    * Update the user input form to load an external js and css

--- main.py
+++ main.py
@@ -7,16 +7,5 @@ def entry():
     return render_template('user_input.html')
 
 
-@app.route('/results', methods=['POST', 'GET'])
-def square_me():
-    if request.method == 'POST':
-        response = {
-            "num1": int(request.form['num1']),
-            "num2": int(request.form['num2']),
-            "operators": request.form["operators"]
-        }
-        return render_template("results.html", response=response)
-
-
 if __name__ == "__main__":
     app.run(debug=True)
